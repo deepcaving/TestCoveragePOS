e75afc3b49d18caedea0f63b437dcb0f071c49bd
Francesco.Capuzzo <francesco.capuzzo@ness.com>
2023-02-20 14:32:22 +0200

    removed top default text

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,6 @@ import streamlit as st
 import os
 import requests
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
 placeholderTxt = "Type the number here"
 st.subheader("Please enter a :blue[Test number] or a :blue[Screen number] from Overflow")
